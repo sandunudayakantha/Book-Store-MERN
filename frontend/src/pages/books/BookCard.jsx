8366a3fe5a904caf3b41acb03d66ebd7917b1719
sandunudayakantha <sndnudayakantha@gmail.com>
2025-01-24 18:16:41 +0530

    top seller section updated

--- frontend/src/pages/books/BookCard.jsx
+++ frontend/src/pages/books/BookCard.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
 import { FiShoppingCart } from "react-icons/fi";
+import { getImgUrl } from '../../utils/getImgUrl';
+
+import { Link } from 'react-router'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 const BookCard = ({book}) => {
   return (
-    <div>
-        <div className='flex items-center'>
+    <div >
+        <div className='flex items-center mb-4 hover:shadow-lg rounded-md p-1'>
 
-            <div>img</div>
+            <div className='sm:h-72 sm:flex-shrink-0 border rounded-md'>
+            <Link to={`/books/${book._id}`}>
+                <img className='w-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200'
+                src={`${getImgUrl(book?.coverImage)}`}
+                />
+                </Link>
+            </div>
 
             <div className='px-3 py-4'>
               
-                <h3 className='mb-3 font-semibold text-lg'>Title</h3>
+              <Link to={`/books/${book._id}`}>
+                <h3 className='mb-3 font-semibold text-lg'>{book?.title}</h3>
+            </Link>
+                <p className='mb-5 text-gray-600'>{book?.description.length > 80 ? `${book.description.slice(0,80)}...`:book?.description}</p>
 
-                <p className='mb-5 text-gray-600'>Discription</p>
+                <p className='font-medium mb-5'>$ {book?.newPrice}  <span className='font-normal ml-1 line-through'>$ {book?.oldPrice}</span></p>
 
-                <p className='font-medium mb-5'>Price  <span className='font-normal ml-1 line-through'>old price</span></p>
-
-                <button className='my-5 bg-yellow-300 px-12 py-2 rounded-lg border border-yellow-700 hover:bg-amber-500 flex items-center gap-2'> <FiShoppingCart /> Add to Cart</button>
+                <button className='md:my-5 my-5 bg-yellow-300 md:px-2 px-6 md:py-2 py-2 rounded-lg border border-yellow-700 hover:bg-amber-500 flex items-center gap-2'> <FiShoppingCart /> Add to Cart</button>
                
             </div>
        </div>
